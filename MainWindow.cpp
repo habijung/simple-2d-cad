ac03b5a15482ad1a81c03339e850fb3c48ac8a9a
habi <habijung0@gmail.com>
2024-03-23 20:39:53 +0900

    feat: File 하위 항목에 메뉴 추가

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -15,9 +15,18 @@ MainWindow::~MainWindow()
 
 void MainWindow::setMenuBar()
 {
+	QAction* newa = new QAction("&New", this);
+	QAction* open = new QAction("&Open", this);
 	QAction* quit = new QAction("&Quit", this);
+	quit->setShortcut(tr("CTRL+Q"));
+
 	QMenu* file = menuBar()->addMenu("&File");
+	file->addAction(newa);
+	file->addAction(open);
+	file->addSeparator();
 	file->addAction(quit);
+
+	qApp->setAttribute(Qt::AA_DontShowIconsInMenus, false);  // Icons are no longer shown in menus
 
 	connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
