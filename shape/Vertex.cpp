a91be525f69e9764f7b9294f1e4c5bca18d45793
habi <habijung0@gmail.com>
2024-03-23 20:40:40 +0900

    refactor: Shape 하위 class 분리

---
+++ shape/Vertex.cpp
@@ -0,0 +1,44 @@
+#include "Shape.h"
+
+
+Vertex::Vertex(float x, float y)
+{
+	mType = "Point";
+	mX = x;
+	mY = y;
+}
+
+Vertex::Vertex(QPointF p)
+{
+	mType = "Point";
+	mX = p.x();
+	mY = p.y();
+}
+
+QPointF Vertex::retVertex()
+{
+	return QPointF(mX, mY);
+}
+
+void Vertex::updateVertex(QPointF p)
+{
+	mX = p.x();
+	mY = p.y();
+}
+
+string Vertex::type()
+{
+	return mType;
+}
+
+bool Vertex::checkType(string s)
+{
+	return !mType.compare(s);
+}
+
+void Vertex::render(QPainter* painter, Camera* camera)
+{
+	QPoint p = camera->setScreen(QPointF(mX, mY));
+	painter->setPen(QPen(Qt::blue, 10, Qt::SolidLine, Qt::RoundCap));
+	painter->drawPoint(p);
+}
