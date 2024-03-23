63b8b42fea0389041835d447cda12bb4b854b9e3
habi <habijung0@gmail.com>
2024-03-23 20:39:46 +0900

    feat: Set window title, size

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+	QApplication a(argc, argv);
+	MainWindow w;
+
+	w.resize(600, 400);
+	w.setWindowTitle("Simple 2D CAD");
+	w.show();
+
+	return a.exec();
 }
