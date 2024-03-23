b872914d23efb64c01ca907394916f83af423447
habi <habijung0@gmail.com>
2024-03-23 20:39:46 +0900

    feat: Create menu bar with quit

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,13 +1,19 @@
 #include "MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindowClass())
+MainWindow::MainWindow(QWidget* parent)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindowClass())
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+
+	QAction* quit = new QAction("&Quit", this);
+	QMenu* file = menuBar()->addMenu("&File");
+	file->addAction(quit);
+
+	connect(quit, &QAction::triggered, qApp, QApplication::quit);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
